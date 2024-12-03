78d020381a6767a02343520d733426242fd9c1d1
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-03 23:50:07 +0530

    [Tanmay]Add: Checking Employee Attendance

---
+++ CheckEmployee.js
@@ -0,0 +1,15 @@
+const attendance = () =>{
+    const randomValue = Math.floor(Math.random()*2);    // either value to be 0 or 1.
+    let isPresent;
+
+    if(randomValue === 0){
+        //assign true if present
+        isPresent = true;        
+    }else{
+        // assign false if absent
+        isPresent = false;
+    }
+
+    return isPresent;
+}
+console.log(attendance());
