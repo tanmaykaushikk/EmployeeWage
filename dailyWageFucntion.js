19efb0049587dc7e7bb8b6db2bc9721ac8f0809c
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-04 00:19:13 +0530

    [Tanmay]Add: Total Working Hours

---
+++ dailyWageFucntion.js
@@ -0,0 +1,8 @@
+const totalTime = Math.floor(Math.random()*8); // total working hours from 0 to 8;
+let workingHours;
+
+function totalWorkingHours(totalTime){
+    workingHours = totalTime;
+    console.log(workingHours);
+}
+totalWorkingHours(totalTime);
