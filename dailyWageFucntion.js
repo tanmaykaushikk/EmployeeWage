c2ed52b0ca5d31432ceeb4a83b348ab811b65275
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-04 11:49:43 +0530

    file adjustment

--- dailyWageFucntion.js
+++
@@ -1,8 +0,0 @@
-const totalTime = Math.floor(Math.random()*8); // total working hours from 0 to 8;
-let workingHours;
-
-function totalWorkingHours(totalTime){
-    workingHours = totalTime;
-    console.log(workingHours);
-}
-totalWorkingHours(totalTime);
