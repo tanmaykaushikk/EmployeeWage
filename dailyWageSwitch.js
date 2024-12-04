d76100dfa92c033668958d195c6256330a31206d
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-04 11:44:05 +0530

    file adjustments

--- dailyWageSwitch.js
+++ dailyWageSwitch.js
@@ -2,6 +2,22 @@ const partTime = 4;
 const fullTime = 8;
 const wagePerHour = 20;
 
+const attendance = () =>{
+    const randomValue = Math.floor(Math.random()*2);    // either value to be 0 or 1.
+    let isPresent;
+
+    if(randomValue === 0){
+        //assign true if present
+        isPresent = true;        
+    }else{
+        // assign false if absent
+        isPresent = false;
+    }
+
+    return isPresent;
+}
+console.log(attendance());
+
 function calculateDailyWage(){
 
     const totalTime = Math.floor(Math.random()*3);   // 0: No Time  , 1: Part Time , 2: Full Time
